589aa27370f845770fc83413cc3105973ed478eb
Sam Ko <sam@vercel.com>
2023-08-02 15:46:13 -0700

    Update /api/[...parts].ts

--- pages/api/[...parts].ts
+++ pages/api/[...parts].ts
@@ -4,10 +4,13 @@ export const config = {
   runtime: "edge",
 };
 
-export default function handler(req: NextRequest) {
+export default function handler(request: NextRequest) {
   return NextResponse.json(
     {
-      parts: req.nextUrl.searchParams.getAll("parts"),
+      request: {
+        nextUrl: request.nextUrl
+      },
+      context: request
     },
     { status: 200 }
   );
