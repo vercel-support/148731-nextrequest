c0aab539a3acd7db70b5fb36526be0ecd52fd5ab
Sam Ko <sam@vercel.com>
2023-08-02 16:09:45 -0700

    Update API Route to send rawHeaders

--- pages/api/[...parts].ts
+++ pages/api/[...parts].ts
@@ -1,9 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  console.log('[test] req =', req)
   res.status(200).send({
     request: {
-      url: req.url
+      url: req.url,
+      rawHeaders: req.rawHeaders
     }
   });
 };
